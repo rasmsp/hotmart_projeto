97e4d91458feee2748701dd1458d6de0a024f89a
Ricardo Araújo <ricardo.araujo@jrmendonca.com.br>
2021-03-10 23:18:35 -0300

    removido imagens do react, alterado descrição do cabeçalho

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,46 @@
-import React,{ useState } from 'react';
-import {Row, Col } from 'react-bootstrap';
-import RoutineName from "../../components/routine_name";
-import Header from './header'
-import Form from './form'
-import Status from './status'
-import Timeline from './timeline'
+import React, { useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import RoutineName from '../../components/routine_name';
+import Header from './header';
+import Form from './form';
+import Status from './status';
+import Timeline from './timeline';
 
 export default function Request() {
-    const [expenseVisible, setExpenseVisible] = useState(false);  
-    const [newExpense, setNewExpense] = useState({}); 
+  const [expenseVisible, setExpenseVisible] = useState(false);
+  const [newExpense, setNewExpense] = useState({});
 
-    const expenseFormSet =() =>{
-      setExpenseVisible(!expenseVisible);     
-   }
+  const expenseFormSet = () => {
+    setExpenseVisible(!expenseVisible);
+  };
 
-   const newExpenseFormSet = (newExpense) =>{
-     setNewExpense(newExpense);  
-   }
+  const newExpenseFormSet = (newExpense) => {
+    setNewExpense(newExpense);
+  };
 
-    return (
-
-      <>
-      <RoutineName menu='Paniel' menu_1='Solicitação' menu_2='Solicitação'  />
-      <div className="container-fluid">       
+  return (
+    <>
+      <RoutineName menu="Painel" menu_1="Solicitação" menu_2="Solicitação" />
+      <div className="container-fluid">
         <Row>
           <Col md="8">
-              <Header expenseVisible={expenseVisible} expenseFormSet={expenseFormSet}/>
-              {expenseVisible &&
-                <Form newExpenseFormSet={newExpenseFormSet} expenseFormSet={expenseFormSet}/>
-               
-              }
-              <Timeline newExpense={newExpense}/>
+            <Header
+              expenseVisible={expenseVisible}
+              expenseFormSet={expenseFormSet}
+            />
+            {expenseVisible && (
+              <Form
+                newExpenseFormSet={newExpenseFormSet}
+                expenseFormSet={expenseFormSet}
+              />
+            )}
+            <Timeline newExpense={newExpense} />
           </Col>
-          <Col md="4">                
-              <Status/>
-            </Col>
+          <Col md="4">
+            <Status />
+          </Col>
         </Row>
-     </div>
-      </> 
-      
-    );
-  }
-  
-  
-  
+      </div>
+    </>
+  );
+}
